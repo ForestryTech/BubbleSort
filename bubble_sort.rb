ab54624c36de4d39bb0e2461b769f59b861b27a3
ForestryTech <jpeterson1973@gmail.com>
2016-05-05 09:21:41 -0700

    Finished bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,25 +1,50 @@
 def bubble_sort(arr)
 
     ctr = 0
-    indx = 1
+    indx = 0
     swap = true
     sort_len = arr.length
-    while ctr < arr.length - 1 && swap === true
+    while swap === true
         swap = false
-        while indx < sort_len
-            if arr[ctr] > arr[indx]
-                temp = arr[ctr]
-                arr[ctr] = arr[indx]
-                arr[indx] = temp
+        while indx < sort_len - 1
+            #print "indx=#{indx} arr[indx]=#{arr[indx]} -- arr[indx + 1]=#{arr[indx + 1]}\n"
+            if arr[indx] > arr[indx + 1]
+                temp = arr[indx]
+                arr[indx] = arr[indx + 1]
+                arr[indx + 1] = temp
                 swap = true
             end
             indx += 1
         end
-        ctr += 1
-        indx = ctr + 1
+        indx = 0
+        sort_len -= 1
     end
     return arr
 end
 
+def bubble_sort_by(arr)
+    ctr = 0
+    indx = 0
+    swap = true
+    sort_len = arr.length
+    
+    while swap === true
+        swap = false
+        while indx < sort_len - 1
+            if yield(arr[indx], arr[indx + 1]) < 0
+                temp = arr[indx]
+                arr[indx] = arr[indx + 1]
+                arr[indx + 1] = temp
+                swap = true
+            end
+            indx += 1
+        end
+        indx = 0
+        sort_len -= 1
+    end
+    return arr
+end
 print bubble_sort([4,3,78,2,0,5,88,9,6,10,13,16,71,93,43,19,28,11,14])
+puts
+print bubble_sort_by(["hi", "hello", "hey"]) { |left, right| right.length - left.length}
 puts
